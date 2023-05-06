a228c526e81fd96ad5ab7ed9865767fee85f8b63
Tetsuya Saito <>
2023-05-07 00:17:07 +0900

    publicのファイルコピー

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         404: resolve(__dirname, '404.html'),
+      },
+      output: {
+        dir: 'dist',
+        assetFileNames: 'assets/[name][extname]'
       }
     }
   }
